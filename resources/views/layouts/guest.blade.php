37181ae558af44b6bba7de32afab26acfe69bcb0
judel fintch <judfintch@gmail.com>
2023-05-23 11:08:47 +0200

    fix :remove vite control in the guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,8 +11,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Scripts 
+        @vite(['resources/css/app.css', 'resources/js/app.js'])-->
+        
+        <link href="{{asset('assets/css/app.css')}}" rel="stylesheet" />
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
