82e7bc3b8fdd0a8edef72f1526361b292ce96405
judel fintch <judfintch@gmail.com>
2023-05-30 14:28:57 +0200

    adding fact

--- resources/views/livewire/printdetail/details_dossier.blade.php
+++ resources/views/livewire/printdetail/details_dossier.blade.php
@@ -1,5 +1,29 @@
-<div class="page-wrapper">
+<style>
+       
 
+        @media print {
+            @page {
+                margin: 0;
+            }
+            body * {
+                visibility: hidden;
+            }
+            #contenu, #contenu * {
+                visibility: visible;
+            }
+            #contenu {
+                position: static;
+                max-width: none;
+                padding: 0;
+                background-color: transparent;
+                box-shadow: none;
+                margin-top: -130px;
+            }
+        }
+    </style>
+    
+
+<div class="page-wrapper">
     <div class="content container-fluid">
         <div class="page-header">
             <div class="row align-items-center">
@@ -14,17 +38,18 @@
                     <div class="btn-group btn-group-sm">
                         <button class="btn btn-white">CSV</button>
                         <button class="btn btn-white">PDF</button>
-                        <button onclick="window.print()" class="btn btn-white"><i class="fa fa-print fa-lg"></i> Print</button>
+                        <button onclick="imprimerPartie()" class="btn btn-white"><i class="fa fa-print fa-lg"></i> Print</button>
                     </div>
                 </div>
             </div>
         </div>
+       <div>
 
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-12" >
                 <div class="card">
-                    <div id="facture" class="card-body">
-                        <h4 class="payslip-title">{{$dossier->plaque}}/ {{date('y-d')}}</h4>
+                    <div  id="contenu" class="card-body">
+                       
                         <div class="row">
                             <div class="col-sm-6 m-b-20">
                                 <img src="{{asset('assets/img/new-castorms.jpg')}}" class="inv-logo" alt>
@@ -45,6 +70,7 @@
                                 </div>
                             </div>
                         </div>
+                       
                         <div class="row">
                             <div class="col-lg-12 m-b-20">
                                 <ul class="list-unstyled">
@@ -59,73 +85,16 @@
                                 </ul>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div>
-                                    <h4 class="m-b-10"><strong>Entree</strong></h4>
-                                    <table class="table table-bordered">
-                                        <tbody>
-                                            <?php $tot_out=0; $tot_int=0;?>
-                                            @foreach($mouvements as $mvt_int)
-                                                @if($mvt_int->type="int")
-                                                
-                                                <?php $tot_int+=$mvt_int->montant?>
-                                                <tr>
-                                                    <td>
-                                                        <strong>{{$mvt_int->motif}}</strong> <span
-                                                            class="float-end">${{$mvt_int->montant}}</span>
-                                                    </td>
-                                                </tr>
-                                                @endif
-                                            @endforeach
-
-                                            <tr>
-                                                <td><strong>Total Earnings</strong> <span class="float-end"><strong>$
-                                                            {{$tot_out}}</strong></span></td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div>
-                                    <h4 class="m-b-10"><strong>Sorties</strong></h4>
-                                    <table class="table table-bordered">
-                                        <tbody>
-
-                                            @foreach($mouvements as $mvt_out)
-                                            @if($mvt_out->type="out")
-                                            <?php $tot_out+=$mvt_out->montant?>
-
-                                            <tr>
-                                                <td><strong>{{$mvt_out->motif}}</strong> <span class="float-end">$
-                                                        {{$mvt_out->montant}}</span></td>
-                                            </tr>
-                                            @endif
-                                            @endforeach
-                                            <tr>
-                                                <td><strong>Total Deductions</strong> <span class="float-end"><strong>$
-                                                            {{$tot_out}}</strong></span></td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                            <div class="col-sm-12">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                        <h4 class="payslip-title">{{$dossier->plaque}}/ {{date('y-d')}}</h4>
+                       
 
 </div>
-
+</div>
 </div>
 
 <script>
-    var imprimer = window.getElementById['facture'];
-    window.print(imprimer);
+    
+    function imprimerPartie() {
+            window.print();
+        }
 </script>
