d5d0b9b1a0ba69dd85ac9d1f4d191a173707ebc0
judel fintch <judfintch@gmail.com>
2023-05-30 16:12:54 +0200

    addign main css profile

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,236 +1,110 @@
 <div class="header">
-            <div class="header-left">
-                <a href="{{route('dashboard')}}" class="logo">
-                    <img src="{{asset('assets/img/new-castorms.jpg')}}" width="40" height="40" alt="">
+    <div class="header-left">
+        <a href="{{route('dashboard')}}" class="logo">
+            <img src="{{asset('assets/img/new-castorms.jpg')}}" width="40" height="40" alt="">
+        </a>
+    </div>
+
+    <a id="toggle_btn" href="javascript:void(0);">
+        <span class="bar-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
+    </a>
+
+    <div class="page-title-box">
+        <h3>{{ config('app.name', 'New Castoms') }}</h3>
+    </div>
+
+    <a id="mobile_btn" class="mobile_btn" href="#sidebar"><i class="fa fa-bars"></i></a>
+
+    <ul class="nav user-menu">
+
+        <li class="nav-item">
+            <div class="top-nav-search">
+                <a href="javascript:void(0);" class="responsive-search">
+                    <i class="fa fa-search"></i>
                 </a>
+                <form action="search">
+                    <input class="form-control" type="text" placeholder="Search here">
+                    <button class="btn" type="submit"><i class="fa fa-search"></i></button>
+                </form>
             </div>
+        </li>
 
-            <a id="toggle_btn" href="javascript:void(0);">
-                <span class="bar-icon">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </span>
+
+        <li class="nav-item dropdown has-arrow flag-nav">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button">
+                <img src="{{asset('assets/img/flags/us.png')}}" alt="" height="20"> <span>English</span>
             </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <a href="javascript:void(0);" class="dropdown-item">
+                    <img src="{{asset('assets/img/flags/us.png')}}" alt="" height="16"> English
+                </a>
+                <a href="javascript:void(0);" class="dropdown-item">
+                    <img src="{{asset('assets/img/flags/fr.png')}}" alt="" height="16"> French
+                </a>
 
-            <div class="page-title-box">
-                <h3>{{ config('app.name', 'New Castoms') }}</h3>
             </div>
-
-            <a id="mobile_btn" class="mobile_btn" href="#sidebar"><i class="fa fa-bars"></i></a>
-
-            <ul class="nav user-menu">
-
-                <li class="nav-item">
-                    <div class="top-nav-search">
-                        <a href="javascript:void(0);" class="responsive-search">
-                            <i class="fa fa-search"></i>
-                        </a>
-                        <form action="search">
-                            <input class="form-control" type="text" placeholder="Search here">
-                            <button class="btn" type="submit"><i class="fa fa-search"></i></button>
-                        </form>
-                    </div>
-                </li>
+        </li>
 
 
-                <li class="nav-item dropdown has-arrow flag-nav">
-                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button">
-                        <img src="assets/img/flags/us.png" alt="" height="20"> <span>English</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a href="javascript:void(0);" class="dropdown-item">
-                            <img src="assets/img/flags/us.png" alt="" height="16"> English
-                        </a>
-                        <a href="javascript:void(0);" class="dropdown-item">
-                            <img src="assets/img/flags/fr.png" alt="" height="16"> French
-                        </a>
-                       
-                    </div>
-                </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
+                <i class="fa fa-bell-o"></i> <span class="badge rounded-pill">3</span>
+            </a>
+            <div class="dropdown-menu notifications">
+                <div class="topnav-dropdown-header">
+                    <span class="notification-title">Notifications</span>
+                    <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
+                </div>
+
+                <div class="topnav-dropdown-footer">
+                    <a href="activities">View all Notifications</a>
+                </div>
+            </div>
+        </li>
 
 
-                <li class="nav-item dropdown">
-                    <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
-                        <i class="fa fa-bell-o"></i> <span class="badge rounded-pill">3</span>
-                    </a>
-                    <div class="dropdown-menu notifications">
-                        <div class="topnav-dropdown-header">
-                            <span class="notification-title">Notifications</span>
-                            <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
-                        </div>
-                        <div class="noti-content">
-                            <ul class="notification-list">
-                                <li class="notification-message">
-                                    <a href="activities">
-                                        <div class="media d-flex">
-                                            <span class="avatar flex-shrink-0">
-                                                <img alt="" src="assets/img/profiles/avatar-02.jpg">
-                                            </span>
-                                            <div class="media-body flex-grow-1">
-                                                <p class="noti-details"><span class="noti-title">John Doe</span> added
-                                                    new task <span class="noti-title">Patient appointment booking</span>
-                                                </p>
-                                                <p class="noti-time"><span class="notification-time">4 mins ago</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                
-                                
-                                <li class="notification-message">
-                                    <a href="activities">
-                                        <div class="media d-flex">
-                                            <span class="avatar flex-shrink-0">
-                                                <img alt="" src="assets/img/profiles/avatar-17.jpg">
-                                            </span>
-                                            <div class="media-body flex-grow-1">
-                                                <p class="noti-details"><span class="noti-title">Rolland Webber</span>
-                                                    completed task <span class="noti-title">Patient and Doctor video
-                                                        conferencing</span></p>
-                                                <p class="noti-time"><span class="notification-time">12 mins ago</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                
-                            </ul>
-                        </div>
-                        <div class="topnav-dropdown-footer">
-                            <a href="activities">View all Notifications</a>
-                        </div>
-                    </div>
-                </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
+                <i class="fa fa-comment-o"></i> <span class="badge rounded-pill">8</span>
+            </a>
+            <div class="dropdown-menu notifications">
+                <div class="topnav-dropdown-header">
+                    <span class="notification-title">Messages</span>
+                    <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
+                </div>
+                <div class="noti-content">
+                    <ul class="notification-list">
 
 
-                <li class="nav-item dropdown">
-                    <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
-                        <i class="fa fa-comment-o"></i> <span class="badge rounded-pill">8</span>
-                    </a>
-                    <div class="dropdown-menu notifications">
-                        <div class="topnav-dropdown-header">
-                            <span class="notification-title">Messages</span>
-                            <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
-                        </div>
-                        <div class="noti-content">
-                            <ul class="notification-list">
-                                <li class="notification-message">
-                                    <a href="apps-chat">
-                                        <div class="list-item">
-                                            <div class="list-left">
-                                                <span class="avatar">
-                                                    <img alt="" src="assets/img/profiles/avatar-09.jpg">
-                                                </span>
-                                            </div>
-                                            <div class="list-body">
-                                                <span class="message-author">Richard Miles </span>
-                                                <span class="message-time">12:28 AM</span>
-                                                <div class="clearfix"></div>
-                                                <span class="message-content">Lorem ipsum dolor sit amet, consectetur
-                                                    adipiscing</span>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="notification-message">
-                                    <a href="apps-chat">
-                                        <div class="list-item">
-                                            <div class="list-left">
-                                                <span class="avatar">
-                                                    <img alt="" src="assets/img/profiles/avatar-02.jpg">
-                                                </span>
-                                            </div>
-                                            <div class="list-body">
-                                                <span class="message-author">John Doe</span>
-                                                <span class="message-time">6 Mar</span>
-                                                <div class="clearfix"></div>
-                                                <span class="message-content">Lorem ipsum dolor sit amet, consectetur
-                                                    adipiscing</span>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="notification-message">
-                                    <a href="apps-chat">
-                                        <div class="list-item">
-                                            <div class="list-left">
-                                                <span class="avatar">
-                                                    <img alt="" src="assets/img/profiles/avatar-03.jpg">
-                                                </span>
-                                            </div>
-                                            <div class="list-body">
-                                                <span class="message-author"> Tarah Shropshire </span>
-                                                <span class="message-time">5 Mar</span>
-                                                <div class="clearfix"></div>
-                                                <span class="message-content">Lorem ipsum dolor sit amet, consectetur
-                                                    adipiscing</span>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="notification-message">
-                                    <a href="apps-chat">
-                                        <div class="list-item">
-                                            <div class="list-left">
-                                                <span class="avatar">
-                                                    <img alt="" src="assets/img/profiles/avatar-05.jpg">
-                                                </span>
-                                            </div>
-                                            <div class="list-body">
-                                                <span class="message-author">Mike Litorus</span>
-                                                <span class="message-time">3 Mar</span>
-                                                <div class="clearfix"></div>
-                                                <span class="message-content">Lorem ipsum dolor sit amet, consectetur
-                                                    adipiscing</span>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="notification-message">
-                                    <a href="apps-chat">
-                                        <div class="list-item">
-                                            <div class="list-left">
-                                                <span class="avatar">
-                                                    <img alt="" src="assets/img/profiles/avatar-08.jpg">
-                                                </span>
-                                            </div>
-                                            <div class="list-body">
-                                                <span class="message-author"> Catherine Manseau </span>
-                                                <span class="message-time">27 Feb</span>
-                                                <div class="clearfix"></div>
-                                                <span class="message-content">Lorem ipsum dolor sit amet, consectetur
-                                                    adipiscing</span>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="topnav-dropdown-footer">
-                            <a href="apps-chat">View all Messages</a>
-                        </div>
-                    </div>
-                </li>
 
-                <li class="nav-item dropdown has-arrow main-drop">
-                    <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
-                        <span class="user-img"><img src="assets/img/profiles/avatar-21.jpg" alt="">
-                            <span class="status online"></span></span>
-                        <span>Admin</span>
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="{{ route('profile.show') }}">My Profile</a>
-                        <a class="dropdown-item" href="settings">Settings</a>
-                        <form method="POST" action="{{ route('logout') }}" x-data>
-                         @csrf
-                            <a class="dropdown-item" href="{{ route('logout') }}" @click.prevent="$root.submit();"> {{ __('Log Out') }}</a>
-                        </form>
-                    </div>
-                </li>
-            </ul>
+                    </ul>
+                </div>
+                <div class="topnav-dropdown-footer">
+                    <a href="apps-chat">View all Messages</a>
+                </div>
+            </div>
+        </li>
 
-        </div>
+        <li class="nav-item dropdown has-arrow main-drop">
+            <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
+                <span class="user-img"><img src="assets/img/profiles/avatar-21.jpg" alt="">
+                    <span class="status online"></span></span>
+                <span> {{ Auth::user()->name }}</span>
+            </a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="{{ route('profile.show') }}">Profile</a>
+               
+                <form method="POST" action="{{ route('logout') }}" x-data>
+                    @csrf
+                    <a class="dropdown-item" href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                        {{ __('Log Out') }}</a>
+                </form>
+            </div>
+        </li>
+    </ul>
 
-
+</div>
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+<link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
