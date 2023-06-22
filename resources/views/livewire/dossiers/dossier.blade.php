d363a665bfacb5fb33edb3be82274eceb22b07fe
judel fintch <judfintch@gmail.com>
2023-06-22 14:10:29 +0200

    fix: adding alert update

--- resources/views/livewire/dossiers/dossier.blade.php
+++ resources/views/livewire/dossiers/dossier.blade.php
@@ -9,7 +9,7 @@
                         <h3 class="page-title">Informations Dossiers</h3>
                         <ul class="breadcrumb">
                             <li class="breadcrumb-item"><a href="dashboard">Dashboard</a></li>
-                            <li class="breadcrumb-item active"></li>
+                            <li class="breadcrumb-item active"><b class="alert alert-danger">Mis a jour encours</b></li>
                         </ul>
                     </div>
                     <div class="col-auto float-end ms-auto">
@@ -20,18 +20,18 @@
             </div>
 
             <div class="row">
-                <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+                <div class="col-md-6 col-sm-6 col-lg-6 col-xl-4">
                     <div class="card dash-widget">
                         <div class="card-body">
                             <span class="dash-widget-icon"><i class="fa fa-cubes"></i></span>
                             <div class="dash-widget-info">
                                 <h3>{{$dossier_day}}</h3>
-                                <span>Dossier(s) du jour</span>
+                                <span>Dossier(s) du jour({{$bigin_date}})</span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+                <div class="col-md-6 col-sm-6 col-lg-6 col-xl-4">
                     <div class="card dash-widget">
                         <div class="card-body">
                             <span class="dash-widget-icon"><i class="fa fa-usd"></i></span>
@@ -43,28 +43,18 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+                <div class="col-md-6 col-sm-6 col-lg-6 col-xl-4">
                     <div class="card dash-widget">
                         <div class="card-body">
                             <span class="dash-widget-icon"><i class="fa fa-diamond"></i></span>
                             <div class="dash-widget-info">
                                 <h3>0</h3>
-                                <span>Solde Negatif <b class=" alert-danger">-</b> </span>
+                                <span>Dossier avec Solde Negatif  </span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
-                    <div class="card dash-widget">
-                        <div class="card-body">
-                            <span class="dash-widget-icon"><i class="fa fa-user"></i></span>
-                            <div class="dash-widget-info">
-                                <h3></h3>
-                                <span>Mouvement Jour</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+               
             </div>
           
 
