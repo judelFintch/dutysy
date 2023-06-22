b6f6ff4c577acb382551c7ee06f7ab800175aeb5
judel fintch <judfintch@gmail.com>
2023-06-22 13:51:38 +0200

    feat :adding new tabs rapport folder

--- app/Http/Livewire/Dossiers/Dossier.php
+++ app/Http/Livewire/Dossiers/Dossier.php
@@ -9,152 +9,154 @@ use App\Models\Clients;
 use App\Models\Destinations;
 use App\Models\Caisses;
 
-class Dossier extends Component
-{
-public $client, $destination,$creat=false,$type_marchandise, $chauffeur, $plaque, $provenance, $montant_init;
-public $selected_id, $search;
-public $update_dossier = false,$idcount=0,$list=true;
+    class Dossier extends Component
+    {
+            public $client, $destination,$creat=false,$type_marchandise, $chauffeur, $plaque, $provenance, $montant_init;
+            public $selected_id, $search;
+            public $update_dossier = false,$idcount=0,$list=true;
+            public $bigin_date,$end_date;
 
-protected $updatesQueryString = ['search'];
+    protected $updatesQueryString = ['search'];
 
-public function mount(){
-$this->search = request()->query('search', $this->search);
-}
-protected $rules = [
-'client' => 'required',
-'destination' => 'required',
-'type_marchandise' => 'required',
-'chauffeur' => 'required',
-'plaque' => 'required',
-'provenance' => 'required',
-'montant_init' => 'required'
-];
+        public function mount(){
+                 $this->search = request()->query('search', $this->search);
+                 $this->bigin_date = date('Y-m-d');
+                 $this->end_date = date('Y-m-d');
+        }
+        protected $rules = [
+                'client' => 'required',
+                'destination' => 'required',
+                'type_marchandise' => 'required',
+                'chauffeur' => 'required',
+                'plaque' => 'required',
+                'provenance' => 'required',
+                'montant_init' => 'required'
+        ];
 
-public function showform(){
-$this->creat =true;
-$this->list =false;
-}
+        public function showform(){
+                $this->creat =true;
+                $this->list =false;
+                }
 
-public function render()
-{
-$search = '%'.$this->search.'%';
-
-$clients = Clients::all();
-$destinations = Destinations::all();
-$dossiers = Dossiers::orderBy('id', 'DESC')->get();
-return view('livewire.dossiers.dossier', compact('dossiers', 'destinations', 'clients', 'dossiers'));
-}
-
-
-private function resetInput(){
-$this->client = null;
-$this->destination = null;
-$this->type_marchandise = null;
-$this->chauffeur = null;
-$this->plaque = null;
-$this->provenance = null;
-$this->montant_init = null;
-}
-public function reinit(){
-$this->resetInput();
-}
+        public function render()
+        {
+                $search = '%'.$this->search.'%';
+                $clients = Clients::all();
+                $destinations = Destinations::all();
+                $dossier_day = Dossiers ::whereDate('created_at',$this->bigin_date)->count();
+                $dossiers = Dossiers::orderBy('id', 'DESC')->get();
+                $dossiers_close = Dossiers ::where('status', 0)->count();
+                return view('livewire.dossiers.dossier', compact('dossiers', 'destinations', 'clients', 'dossiers','dossier_day','dossiers_close'));
+        }
 
 
-public function store()
-{
-//$validatedData =
-$this->validate();
-
-try{
-$dossier =Dossiers::create([
-'client_id' => $this->client,
-'destination_id' => $this->destination,
-'type_marchandise' => $this->type_marchandise,
-'chauffeur' => $this->chauffeur,
-'plaque' => $this->plaque,
-'provenance' => $this->provenance,
-'montant_init' => $this->montant_init,
-'status' => true
-]);
+        private function resetInput(){
+                $this->client = null;
+                $this->destination = null;
+                $this->type_marchandise = null;
+                $this->chauffeur = null;
+                $this->plaque = null;
+                $this->provenance = null;
+                $this->montant_init = null;
+        }
+        public function reinit(){
+                   $this->resetInput();
+        }
 
 
+        public function store()
+        {
+        //$validatedData =
+        $this->validate();
 
-//a mettre dans une fonction  update
-$search ='Dossier';
-$caisse = Caisses::where('name_caisse', 'like', $search )->first();
-if ($caisse){
-$old_mt=$caisse->montant;
-$nw_mt = $old_mt +$this->montant_init;
-$caisse->montant = $nw_mt;
-$caisse->save();
-}
+                try{
+                $dossier =Dossiers::create([
+                        'client_id' => $this->client,
+                        'destination_id' => $this->destination,
+                        'type_marchandise' => $this->type_marchandise,
+                        'chauffeur' => $this->chauffeur,
+                        'plaque' => $this->plaque,
+                        'provenance' => $this->provenance,
+                        'montant_init' => $this->montant_init,
+                        'status' => true
+                ]);
 
-Mouvements::create(
-[
-'dossier_id' => $dossier->id,
-'type' => 'int',
-'libelle' => 'Ouverture Dossier',
-'montant' => $this->montant_init,
-'motif' => 'Ouverture Dossier',
-'beneficiaire' => 'Caisse Dossier',
-'observation' => '---',
-'caisse_id' => $caisse->id
-]
-); 
+        //a mettre dans une fonction  update
+        $search ='Dossier';
+        $caisse = Caisses::where('name_caisse', 'like', $search )->first();
+        if ($caisse){
+                $old_mt=$caisse->montant;
+                $nw_mt = $old_mt +$this->montant_init;
+                $caisse->montant = $nw_mt;
+                $caisse->save();
+        }
 
-session()->flash('message', 'operation reussi');
-$this->resetInput();
-$this->creat=false;
-$this->list=true;
+        Mouvements::create(
+                [
+                'dossier_id' => $dossier->id,
+                'type' => 'int',
+                'libelle' => 'Ouverture Dossier',
+                'montant' => $this->montant_init,
+                'motif' => 'Ouverture Dossier',
+                'beneficiaire' => 'Caisse Dossier',
+                'observation' => '---',
+                'caisse_id' => $caisse->id
+        ]
+        ); 
 
-}
-catch(\Exception $e) {
-dd($e);
-}
+        session()->flash('message', 'operation reussi');
+                $this->resetInput();
+                $this->creat=false;
+                $this->list=true;
+
+        }
+        catch(\Exception $e) {
+                 dd($e);
+        }
 
 
-}
+        }
 
-public function edit($id)
-{
-$record = Dossiers::findOrFail($id);
-$this->selected_id = $id;
-$this->client_id = $record->client_id;
-$this->destination_id = $record->destination_id;
-$this->type_marchandise = $record->type_marchandise;
-$this->chauffeur = $record->chauffeur;
-$this->plaque = $record->plaque;
-$this->provenance = $record->provenance;
-$this->montant_init = $record->montant_init;
-$this->update_dossier = true;
-}
+        public function edit($id)
+        {
+                $record = Dossiers::findOrFail($id);
+                $this->selected_id = $id;
+                $this->client_id = $record->client_id;
+                $this->destination_id = $record->destination_id;
+                $this->type_marchandise = $record->type_marchandise;
+                $this->chauffeur = $record->chauffeur;
+                $this->plaque = $record->plaque;
+                $this->provenance = $record->provenance;
+                $this->montant_init = $record->montant_init;
+                $this->update_dossier = true;
+        }
 
-public function update(){
-$validatedData = $this->validate([
-'client_id' => 'required',
-'destination_id' => 'required',
-'type_marchandise' => 'required',
-'chauffeur' => 'required',
-'plaque' => 'required',
-'provenance' => 'required',
-'montant_init' => 'required'
-]);
-$record = Dossiers::find($this->selected_id);
-// $record->caisse->amount = $validatedData["montant_init"];
-$record->caisse->update(["amount"=> $validatedData["montant_init"]]);
-$record->update($validatedData);
-$this->update_dossier = false;
-// $record->caisses
-$this->resetInput();
-}
+        public function update(){
+                $validatedData = $this->validate([
+                'client_id' => 'required',
+                'destination_id' => 'required',
+                'type_marchandise' => 'required',
+                'chauffeur' => 'required',
+                'plaque' => 'required',
+                'provenance' => 'required',
+                'montant_init' => 'required'
+        ]);
+                $record = Dossiers::find($this->selected_id);
+                // $record->caisse->amount = $validatedData["montant_init"];
+                $record->caisse->update(["amount"=> $validatedData["montant_init"]]);
+                $record->update($validatedData);
+                $this->update_dossier = false;
+                // $record->caisses
+                $this->resetInput();
+        }
 
-public function destroy($id){
-if($id){
-$record = Dossiers::where('id', $id)->first();
-$record->caisse->delete();
-$record->delete();
-}
-}
+        public function destroy($id){
+        if($id){
+                $record = Dossiers::where('id', $id)->first();
+                $record->caisse->delete();
+                $record->delete();
+        }
+    }
 
 
 }
--- resources/views/livewire/dossiers/dossier.blade.php
+++ resources/views/livewire/dossiers/dossier.blade.php
@@ -2,13 +2,14 @@
     <x-nav_left />
     <div class="page-wrapper">
         <div class="content container-fluid">
-            <div class="page-header">
+
+        <div class="page-header">
                 <div class="row align-items-center">
                     <div class="col">
-                        <h3 class="page-title">Dossier</h3>
+                        <h3 class="page-title">Informations Dossiers</h3>
                         <ul class="breadcrumb">
                             <li class="breadcrumb-item"><a href="dashboard">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Dossier</li>
+                            <li class="breadcrumb-item active"></li>
                         </ul>
                     </div>
                     <div class="col-auto float-end ms-auto">
@@ -18,24 +19,73 @@
                 </div>
             </div>
 
-            
-
-                @if (session()->has('message'))
-                <div class="alert alert-success">
-                    {{ session('message') }}
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+                    <div class="card dash-widget">
+                        <div class="card-body">
+                            <span class="dash-widget-icon"><i class="fa fa-cubes"></i></span>
+                            <div class="dash-widget-info">
+                                <h3>{{$dossier_day}}</h3>
+                                <span>Dossier(s) du jour</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                @endif
-                @if($update_dossier)
-                    @include('livewire.dossiers.update')
+                <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+                    <div class="card dash-widget">
+                        <div class="card-body">
+                            <span class="dash-widget-icon"><i class="fa fa-usd"></i></span>
+                            <div class="dash-widget-info">
+                                <h3>{{ $dossiers_close }}</h3>
+                                <span>Dossier(s) Cloturé</span>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+                    <div class="card dash-widget">
+                        <div class="card-body">
+                            <span class="dash-widget-icon"><i class="fa fa-diamond"></i></span>
+                            <div class="dash-widget-info">
+                                <h3>37</h3>
+                                <span>Solde Negatif <b class=" alert-danger">-</b> </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+                    <div class="card dash-widget">
+                        <div class="card-body">
+                            <span class="dash-widget-icon"><i class="fa fa-user"></i></span>
+                            <div class="dash-widget-info">
+                                <h3></h3>
+                                <span>Mouvement Jour</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          
 
-                @endif
-                @if($creat)
-                    @include('livewire.dossiers.creat')
-                @endif
 
-                @if($list)
-                    @include('livewire.dossiers.list')
-                @endif
+
+            @if (session()->has('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+            @endif
+            @if($update_dossier)
+            @include('livewire.dossiers.update')
+
+            @endif
+            @if($creat)
+            @include('livewire.dossiers.creat')
+            @endif
+
+            @if($list)
+            @include('livewire.dossiers.list')
+            @endif
         </div>
     </div>
 </div>
