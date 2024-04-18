5dffddd0f93b40f4daaeb1a274a780d58118f11e
Judel Fintch <judfintch@gmail.com>
2024-04-18 08:15:01 +0200

    feat :adding cdf rapport

--- resources/views/livewire/detailmvt/list.blade.php
+++ resources/views/livewire/detailmvt/list.blade.php
@@ -1,16 +1,17 @@
 <div class="row">
     <div class="col-md-12">
         <div class="table-responsive">
-        <table class="table table-striped custom-table datatable">
+            <table class="table table-striped custom-table datatable">
                 <thead>
                     <tr>
                         <th>#id</th>
                         <th>Date</th>
                         <th>Débit USD</th>
                         <th>Crédit USD</th>
-                        
+                        <th>Débit CDF</th>
+                        <th>Crédit CDF</th>
                         <th>Motif</th>
-                        <th>Observation</th>
+                       
                         <th class="text-end">Bénéficiaire</th>
                         <th class="text-end">Action</th>
                     </tr>
@@ -33,10 +34,21 @@
                                 <td></td>
                                 <td></td>
                             @endif
-                            <!-- Gestion des montants en CDF, vérification de l'existence de otherDetails -->
-                            
+
+                            <!-- Gestion des montants en CDF -->
+                            @if($doss->type == 'int')
+                                <td><span class="badge bg-inverse-info"> + {{ number_format($doss->amount_cdf) }} FC</span></td>
+                                <td></td>
+                            @elseif($doss->type == 'out')
+                                <td></td>
+                                <td><span class="badge bg-inverse-warning"> - {{ number_format($doss->amount_cdf) }} FC</span></td>
+                            @else
+                                <td></td>
+                                <td></td>
+                            @endif
+
                             <td>{{ $doss->motif }}</td>
-                            <td>{{ $doss->observation }}</td>
+                            
                             <td class="text-end">{{ $doss->beneficiaire }}</td>
                             <td class="text-end">
                                 @if($doss->dossier && $doss->dossier->status == 1)
